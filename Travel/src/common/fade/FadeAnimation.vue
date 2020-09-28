988d0af01a940d37324e7c3aeb8acb07166ae696
hwkgithub <2965498304@qq.com>
2020-09-28 15:23:26 +0800

    旅游项目

---
+++ Travel/src/common/fade/FadeAnimation.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Description: 
+ * @Autor: HWK
+ * @Date: 2020-09-16 22:20:03
+ * @LastEditors: HWK
+ * @LastEditTime: 2020-09-28 15:21:30
+-->
+<template>
+  <transition>
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'FadeAnimation',
+}
+</script>
+
+<style lang="stylus" scoped>
+.v-enter, .v-leave-to
+  opacity 0
+.v-enter-active, .v-leave-active
+  transition opacity 0.5s
+</style>
---
+++ Travel/src/pages/city/City.vue
@@ -0,0 +1,63 @@
+<!--
+ * @Description: 
+ * @Autor: HWK
+ * @Date: 2020-09-16 22:20:03
+ * @LastEditors: HWK
+ * @LastEditTime: 2020-09-28 15:12:53
+-->
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search :cities="cities"></city-search>
+    <city-list :cities="cities"
+               :hot="hotCities"
+               :letter="letter"></city-list>
+    <city-alphabet :cities="cities"
+                   @change="handleLetterChange"></city-alphabet>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
+export default {
+  name: 'City',
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet,
+  },
+  data() {
+    return {
+      cities: {},
+      hotCities: [],
+      letter: '',
+    }
+  },
+  methods: {
+    getCityInfo() {
+      axios.get('/api/city.json').then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc(res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    },
+    handleLetterChange(letter) {
+      this.letter = letter
+    },
+  },
+  mounted() {
+    this.getCityInfo()
+  },
+}
+</script>
+
+<style lang="stylus" scoped></style>
